fd71b7ad48c66be825def3fb8d6d50f71b97aa95
Casey <caseyorigami@gmail.com>
2019-03-23 22:32:01 -0700

    Added a page to use the strie, and fixed typos

--- suffixtrie.js
+++ suffixtrie.js
@@ -9,14 +9,15 @@ function CreateSuffixTrie() {
             children: [], // the possible suffixes
             link: null // the failure link for building the suffix trie
         },
-        deep: root
+        deep: null
     };
     strie.root.link = strie.root;
+    strie.deep = strie.root;
 
     strie.append = function( symbol ) {
 
         // this will be our pointer for traversing the previous boundary path
-        let active = deep.link;
+        let active = strie.deep.link;
 
         // find the next deepest suffix after this symbol is added
         strie.deep.children[ symbol ] = { name: symbol, children:[], link:null };
@@ -27,7 +28,7 @@ function CreateSuffixTrie() {
 
         // traverse the previous boundary path, exiting when you encounter work
         // you've already performed on a previous occurance of the symbol
-        while (active.children[ symbol ] == undef) {
+        while (!active.children[ symbol ]) {
 
             // add a transition for the symbol from the old boundary path to the new boundary
             // and link the new node to the boundary path as well
@@ -42,7 +43,7 @@ function CreateSuffixTrie() {
         }
 
         // now we have to attach the new boundary path tp any previously computed path for this suffix
-        if (active.transitions[ symbol ] == suffix)
+        if (active.children[ symbol ] == suffix)
             suffix.link = root;
         else
             suffix.link = active.children[ symbol ];
